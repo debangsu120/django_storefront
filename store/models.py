b9b6a26a0cdff5e06c058999892d646f77f557d6
debangsu120 <debangsuhazarika2005@gmail.com>
2025-08-12 02:46:28 +0530

    birthdate to birth_date for sql huh

--- store/models.py
+++ store/models.py
@@ -38,7 +38,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15,unique=True)
-    birthdate = models.DateField(null = True)
+    birth_date = models.DateField(null = True)
     membership = models.CharField(max_length= 1 , choices=MEMBERSHIP_CHOICES)
 
 
