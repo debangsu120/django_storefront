0eef9fb587186fdef1496478624c964bc8515fd9
debangsu120 <debangsuhazarika2005@gmail.com>
2025-08-13 02:58:38 +0530

    managers,querysets , retrieving objects , filtering objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
-def calculate():
-    x=1
-    y=2
-    return x
 
 def say_hello(request):
-    x= calculate()
-    return render(request , 'hello.html' , { 'name' : 'Sunny'})
+    # query_set = Product.objects.all()
+
+    # for product in query_set:
+    #     print(product)
+    
+    # try:
+    #     product = Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+
+    # product = Product.objects.filter(pk=0).first() #none
+    # exists = Product.objects.filter(pk=0).exists()
+    
+    #keyword=value
+    # queryset = Product.objects.filter(unit_price__range=(20,300))
+    queryset = Product.objects.filter(title__icontains='coffee')
+
+
+    return render(request , 'hello.html' , { 'name' : 'Sunny' , 'products' : list(queryset)})
